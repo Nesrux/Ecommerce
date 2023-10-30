a95144a0d753bfaeedcf143c2d4facdce5c3c547
Nesrux <joaomarcosdevs@gmail.com>
2023-10-30 19:45:29 -0300

    Config: configurando as classes de Teste

--- src/test/java/com/nesrux/ecommerce/iniciandoComJpa/ConsultandoRegistroTest.java
+++ src/test/java/com/nesrux/ecommerce/iniciandoComJpa/ConsultandoRegistroTest.java
@@ -13,22 +13,26 @@ public class ConsultandoRegistroTest {
     private static EntityManagerFactory entityManagerFactory;
     private EntityManager entityManager;
 
+    /*antes da classe ser executada*/
     @BeforeClass
     public static void setUpBeforeClasse() {
         entityManagerFactory = Persistence
                 .createEntityManagerFactory("Ecommerce-PU");
     }
 
+    /*depois da classe ser executada*/
     @AfterClass
     public static void tearDownAfterClasse() {
         entityManagerFactory.close();
     }
 
+    /*antes de cada método*/
     @Before
     public void setUp() {
         entityManager = entityManagerFactory.createEntityManager();
     }
 
+    /*depois de cada método*/
     @After
     public void teardown() {
         entityManager.close();
