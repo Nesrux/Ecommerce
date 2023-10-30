16b692c606d043c250cf93ae3c46a8f3152daaca
Nesrux <joaomarcosdevs@gmail.com>
2023-10-30 19:27:23 -0300

    Feat: adicionando o arquivo de initPErsistence (metodo main)

---
+++ src/main/java/com/nesrux/ecommerce/util/InitPersistence.java
@@ -0,0 +1,20 @@
+package com.nesrux.ecommerce.util;
+
+import com.nesrux.ecommerce.model.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InitPersistence {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Ecommerce-PU");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Produto produto = entityManager.find(Produto.class, 1L);
+        System.out.println(produto.getNome());
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}
