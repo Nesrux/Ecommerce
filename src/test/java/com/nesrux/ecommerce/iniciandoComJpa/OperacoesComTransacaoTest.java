81aeb9281d1e0af0672433550cb42eec604d2269
Nesrux <joaomarcosdevs@gmail.com>
2023-10-31 15:51:53 -0300

    Feat: adicionando teste para inserir no banco de dados

--- src/test/java/com/nesrux/ecommerce/iniciandoComJpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/nesrux/ecommerce/iniciandoComJpa/OperacoesComTransacaoTest.java
@@ -1,20 +1,39 @@
 package com.nesrux.ecommerce.iniciandoComJpa;
 
 import com.nesrux.ecommerce.model.Produto;
+import org.junit.Assert;
 import org.junit.Test;
 import util.EntityManagerTest;
 
+import java.math.BigDecimal;
+
 public class OperacoesComTransacaoTest extends EntityManagerTest {
+
+    @Test
+    public void inserirObjeto() {
+        Produto produto = new Produto();
+        produto.setId(2L);
+        produto.setNome("Câmera");
+        produto.setDescricao("Camera pika");
+        produto.setPreco(new BigDecimal("5000"));
+
+        entityManager.getTransaction().begin();
+
+        entityManager.persist(produto);
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+
+        Produto produtoInserido = entityManager.find(Produto.class, 2L);
+        Assert.assertNotNull(produtoInserido);
+
+
+    }
+
     @Test
     public void abrirEFecharTransacao() {
         Produto produto = new Produto();
         //Inicia a transacao
         entityManager.getTransaction().begin();
-
-        entityManager.persist(produto);
-        entityManager.merge(produto);
-        entityManager.remove(produto);
-
         //Fecha transaçãp
         entityManager.getTransaction().commit();
     }
