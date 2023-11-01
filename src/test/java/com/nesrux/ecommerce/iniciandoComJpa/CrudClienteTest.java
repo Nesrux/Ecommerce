ef7447648995b6576a39904edce0a0b95d35b73e
Nesrux <joaomarcosdevs@gmail.com>
2023-11-01 18:16:03 -0300

    Desafio : desafio CrudCliente completo

--- src/test/java/com/nesrux/ecommerce/iniciandoComJpa/CrudClienteTest.java
+++ src/test/java/com/nesrux/ecommerce/iniciandoComJpa/CrudClienteTest.java
@@ -4,8 +4,7 @@ import com.nesrux.ecommerce.model.Cliente;
 import org.junit.Test;
 import util.EntityManagerTest;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class CrudClienteTest extends EntityManagerTest {
 
@@ -23,6 +22,39 @@ public class CrudClienteTest extends EntityManagerTest {
         assertNotNull(cliente);
 
         assertEquals(cliente, clientePersistido);
+    }
+
+    @Test
+    public void lerCliente() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+        assertNotNull(cliente);
+    }
+
+    @Test
+    public void atualizarCliente() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+        entityManager.getTransaction().begin();
+        cliente.setNome("Moacir");
+        entityManager.merge(cliente);
+
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+
+        assertEquals("Moacir", cliente.getNome());
+    }
+
+    @Test
+    public void deletarCliente() {
+        Cliente cliente = entityManager.find(Cliente.class, 2);
+
+        entityManager.getTransaction().begin();
+        entityManager.remove(cliente);
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+
+        Cliente clienteNulo = entityManager.find(Cliente.class, 2);
+        System.out.println(cliente.getNome());
+        assertNull(clienteNulo);
     }
 
 
