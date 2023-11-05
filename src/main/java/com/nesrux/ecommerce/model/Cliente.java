6715273ff83eba83df594c6ea2ce77a50c4adf23
Nesrux <joaomarcosdevs@gmail.com>
2023-11-05 12:47:46 -0300

    Refactor: refatorando o codigo das entidades, e adicionando o relacionamento de Pedido para cliente

--- src/main/java/com/nesrux/ecommerce/model/Cliente.java
+++ src/main/java/com/nesrux/ecommerce/model/Cliente.java
@@ -14,6 +14,7 @@ public class Cliente {
     @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String nome;
 
     @Enumerated(EnumType.STRING)
