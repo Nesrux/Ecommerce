9566b0a82486191f6467daf51a75b1ac249b1a0b
Nesrux <joaomarcosdevs@gmail.com>
2023-11-05 13:27:35 -0300

    Refactor: refatorando a entidade e adicionando relacionamento com Pedido e Produto

--- src/main/java/com/nesrux/ecommerce/model/ItemPedido.java
+++ src/main/java/com/nesrux/ecommerce/model/ItemPedido.java
@@ -16,13 +16,15 @@ public class ItemPedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name="pedido_id")
-    private Integer pedidoId;
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
 
-    @Column(name="produto_id")
-    private Integer produtoId;
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
 
-    @Column(name="preco_produto")
+    @Column(name = "preco_produto")
     private BigDecimal precoProduto;
 
     private Integer quantidade;
