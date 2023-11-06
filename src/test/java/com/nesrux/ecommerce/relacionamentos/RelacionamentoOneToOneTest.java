8d536c6004df94e7a24cb2f5d154bac34c81f1ad
Nesrux <joaomarcosdevs@gmail.com>
2023-11-06 14:32:13 -0300

    Test: adicionando Teste para relacionamento oneToOne de Pedido com nota ficas

--- src/test/java/com/nesrux/ecommerce/relacionamentos/RelacionamentoOneToOneTest.java
+++ src/test/java/com/nesrux/ecommerce/relacionamentos/RelacionamentoOneToOneTest.java
@@ -1,5 +1,6 @@
 package com.nesrux.ecommerce.relacionamentos;
 
+import com.nesrux.ecommerce.model.NotaFiscal;
 import com.nesrux.ecommerce.model.PagamentoCartao;
 import com.nesrux.ecommerce.model.Pedido;
 import com.nesrux.ecommerce.model.StatusPagamento;
@@ -25,5 +26,24 @@ public class RelacionamentoOneToOneTest extends EntityManagerTest {
         Pedido pedidoPersistido = entityManager.find(Pedido.class, pedido.getId());
         Assert.assertNotNull(pedidoPersistido.getPagamento());
         System.out.println(pedidoPersistido.getPagamento().getNumero());
+    }
+
+    @Test
+    public void verificarRelacionamentoPedidoComNotaFiscal() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        NotaFiscal notaFiscal = new NotaFiscal();
+        notaFiscal.setPedido(pedido);
+        notaFiscal.setXml("<Test> piruleta da silva</test>");
+
+        pedido.setNotaFiscal(notaFiscal);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(notaFiscal);
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+
+        Pedido pedidoPersistido = entityManager.find(Pedido.class, 1);
+        Assert.assertNotNull(pedidoPersistido.getNotaFiscal());
     }
 }
