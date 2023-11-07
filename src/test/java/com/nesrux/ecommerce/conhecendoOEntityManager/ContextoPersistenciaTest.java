7a736da499c36ac069edb6689ecd49989990259c
Nesrux <joaomarcosdevs@gmail.com>
2023-11-07 14:54:02 -0300

    Test: entendendo o contexto de persistencia

--- src/test/java/com/nesrux/ecommerce/conhecendoOEntityManager/ContextoPersistenciaTest.java
+++ src/test/java/com/nesrux/ecommerce/conhecendoOEntityManager/ContextoPersistenciaTest.java
@@ -7,13 +7,18 @@ import util.EntityManagerTest;
 import java.math.BigDecimal;
 
 public class ContextoPersistenciaTest extends EntityManagerTest {
-   @Test
-    public void contextoPersistencia(){
-       Produto produto = entityManager.find(Produto.class, 1);
-       produto.setPreco(new BigDecimal("150.50"));
+    @Test
+    public void contextoPersistencia() {
+        Produto produto = entityManager.find(Produto.class, 1);
+       /*Nesse método, o JPA faz um  dirtCheck para verificar se o objeto foi
+       * modificado, se sim ele da um update no banco de dados*/
+        produto.setPreco(new BigDecimal("150.50"));
 
-       entityManager.getTransaction().begin();
+        entityManager.getTransaction().begin();
 
-       entityManager.getTransaction().commit();
-   }
+        entityManager.getTransaction().commit();
+        /*Os métodos de persist, find e marge colocam esse obj no contexto de persistencia
+         * nesse contexto qualquer alteração feita no objeto quando ocorrer uma transação
+         * ele vai fazer o update no banco de dados onde referencia esse objeto*/
+    }
 }
