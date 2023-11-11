59c91a186dfe62980c7589741aac85906008a77f
Nesrux <joaomarcosdevs@gmail.com>
2023-11-11 16:27:19 -0300

    Test: adicionando Test para salvar arquivos xml no banco de dados

--- src/test/java/com/nesrux/ecommerce/mapeamentoAvancado/SalvandoArquivosTest.java
+++ src/test/java/com/nesrux/ecommerce/mapeamentoAvancado/SalvandoArquivosTest.java
@@ -2,16 +2,29 @@ package com.nesrux.ecommerce.mapeamentoAvancado;
 
 import com.nesrux.ecommerce.model.Pedido.NotaFiscal;
 import com.nesrux.ecommerce.model.Pedido.Pedido;
+import org.junit.Assert;
 import org.junit.Test;
 import util.EntityManagerTest;
 
+import java.io.IOException;
+
 public class SalvandoArquivosTest extends EntityManagerTest {
+
+    private static byte[] carregarNotaFiscal() {
+        try {
+            return SalvandoArquivosTest.class.getResourceAsStream(
+                    "/nota-fiscal.xml").readAllBytes();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Test
     public void salvarXmlBancoDeDados() {
         Pedido pedido = entityManager.find(Pedido.class, 1);
         NotaFiscal notaFiscal = new NotaFiscal();
         notaFiscal.setPedido(pedido);
-
+        notaFiscal.setXml(carregarNotaFiscal());
 
         entityManager.getTransaction().begin();
 
@@ -20,9 +33,10 @@ public class SalvandoArquivosTest extends EntityManagerTest {
         entityManager.getTransaction().commit();
         entityManager.clear();
 
-        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
-
-
+        NotaFiscal notaFiscalVerificacao = entityManager.find(NotaFiscal.class,
+                notaFiscal.getId());
+        Assert.assertNotNull(notaFiscalVerificacao);
+        Assert.assertTrue(notaFiscalVerificacao.getXml().length > 0);
     }
 
 }
