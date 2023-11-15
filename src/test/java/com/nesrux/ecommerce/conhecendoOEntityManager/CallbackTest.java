15e52a59bf6a858279dbd338d7d0d232a125e9d3
Nesrux <joaomarcosdevs@gmail.com>
2023-11-15 19:31:58 -0300

    Fix: resolvendo erro do test cliente not null

--- src/test/java/com/nesrux/ecommerce/conhecendoOEntityManager/CallbackTest.java
+++ src/test/java/com/nesrux/ecommerce/conhecendoOEntityManager/CallbackTest.java
@@ -1,8 +1,8 @@
 package com.nesrux.ecommerce.conhecendoOEntityManager;
 
-import com.nesrux.ecommerce.model.cliente.Cliente;
 import com.nesrux.ecommerce.model.Pedido.Pedido;
 import com.nesrux.ecommerce.model.Pedido.StatusPedido;
+import com.nesrux.ecommerce.model.cliente.Cliente;
 import org.junit.Assert;
 import org.junit.Test;
 import util.EntityManagerTest;
@@ -12,10 +12,14 @@ import java.math.BigDecimal;
 public class CallbackTest extends EntityManagerTest {
     @Test
     public void testarCalbackAtualizacaoECriacao() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+
+
         Pedido pedido = new Pedido();
         pedido.setStatus(StatusPedido.PAGO);
         pedido.setCliente(entityManager.find(Cliente.class, 1));
         pedido.setTotal(BigDecimal.TEN);
+        pedido.setCliente(cliente);
 
         entityManager.getTransaction().begin();
 
