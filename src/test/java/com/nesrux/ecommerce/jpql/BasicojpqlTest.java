a090eebae82e8e8f8af7983661c4ae98e15ae85d
Nesrux <joaomarcosdevs@gmail.com>
2023-11-20 17:54:00 -0300

    Test: adicionando teste para retorno de propriedade em jpql

--- src/test/java/com/nesrux/ecommerce/jpql/BasicojpqlTest.java
+++ src/test/java/com/nesrux/ecommerce/jpql/BasicojpqlTest.java
@@ -1,12 +1,16 @@
 package com.nesrux.ecommerce.jpql;
 
 import com.nesrux.ecommerce.model.Pedido.Pedido;
+import com.nesrux.ecommerce.model.cliente.Cliente;
+import com.nesrux.ecommerce.model.produto.Produto;
 import org.junit.Assert;
 import org.junit.Test;
 import util.EntityManagerTest;
 
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class BasicojpqlTest extends EntityManagerTest {
 
@@ -26,7 +30,7 @@ public class BasicojpqlTest extends EntityManagerTest {
 
     @Test
     public void mostrarDiferencaQuerys() {
-        String jpql = "elect p from Pedido p where p.id=1";
+        String jpql = "select p from Pedido p where p.id=1";
         TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
         Pedido pedidoTipado = typedQuery.getSingleResult();
 
@@ -35,5 +39,19 @@ public class BasicojpqlTest extends EntityManagerTest {
 
         Assert.assertNotNull(pedidoTipado);
         Assert.assertNotNull(pedidoDois);
+    }
+
+    @Test
+    public void selecionarUmAtributo(){
+        String jpql = "select p.nome from Produto p";
+        TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
+        List<String> nomes = typedQuery.getResultList();
+        Assert.assertEquals(String.class, nomes.get(0).getClass());
+
+        String jpqlCliente = "select p.cliente from Pedido p";
+        TypedQuery<Cliente> clientesTypados = entityManager.createQuery(jpqlCliente, Cliente.class);
+        List<Cliente> clientesList = clientesTypados.getResultList();
+
+        Assert.assertEquals(Cliente.class, clientesList.get(0).getClass());
     }
 }
