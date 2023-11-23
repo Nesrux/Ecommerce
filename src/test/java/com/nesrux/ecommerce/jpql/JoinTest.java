e7ca5f772356bde8b94c11457eae7cb8a4915f7a
Nesrux <joaomarcosdevs@gmail.com>
2023-11-23 19:00:20 -0300

    Test: fetch Test

--- src/test/java/com/nesrux/ecommerce/jpql/JoinTest.java
+++ src/test/java/com/nesrux/ecommerce/jpql/JoinTest.java
@@ -1,5 +1,6 @@
 package com.nesrux.ecommerce.jpql;
 
+import com.nesrux.ecommerce.model.Pedido.Pedido;
 import org.junit.Assert;
 import org.junit.Test;
 import util.EntityManagerTest;
@@ -8,6 +9,23 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class JoinTest extends EntityManagerTest {
+
+    @Test
+    public void usarfatch() {
+        /*Fetch faz ele obrigatóriamente carregar as propriedades
+         * mesmo elas sendo leazy*/
+
+        String jpql = "select p from Pedido p " +
+                " left join fetch p.pagamento " +
+                " join fetch p.cliente " +
+                " left join fetch p.notaFiscal";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        List<Pedido> pedidos = typedQuery.getResultList();
+        Assert.assertFalse(pedidos.isEmpty());
+
+    }
+
     @Test
     public void fazerInnerJoinTest() {
         String jpql = "select p from Pedido p join p.pagamento pag";
@@ -27,7 +45,6 @@ public class JoinTest extends EntityManagerTest {
 
         Assert.assertFalse(pedidos.isEmpty());
     }
-
 
 
 }
