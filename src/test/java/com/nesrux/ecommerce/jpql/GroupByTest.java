e8593cf703b5daf5424c506a64cd51a2ff4ef9ef
Nesrux <joaomarcosdevs@gmail.com>
2023-11-28 14:32:20 -0300

    Test: Test para utilizar groupBy juntamente com funções de agregação

--- src/test/java/com/nesrux/ecommerce/jpql/GroupByTest.java
+++ src/test/java/com/nesrux/ecommerce/jpql/GroupByTest.java
@@ -17,9 +17,12 @@ public class GroupByTest extends EntityManagerTest {
 //        String jpql = "select concat(year(p.dataCriacao), function('monthname', 'p.dataCriacao')), " +
 //                "sum(p.total) from Pedido p" +
 //                " group by year(p.dataCriacao), month(p.dataCriacao)";
-        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido" +
-                " ip join ip.produto pro join pro.categorias c" +
-                " group by c.id";
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido" +
+//                " ip join ip.produto pro join pro.categorias c" +
+//                " group by c.id";
+
+        String jpql = "select c.nome, sum(ip.precoProduto) from Pedido p" +
+                " join p.cliente c join p.itens ip group by c.nome";
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
