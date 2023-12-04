066f12772048ca61235ebb14b3da77dde4012558
Nesrux <joaomarcosdevs@gmail.com>
2023-12-04 17:36:05 -0300

    Test: retornando propriedades de entidades com criteria api

--- src/test/java/com/nesrux/ecommerce/criteria/BasicoCriteriaApiTest.java
+++ src/test/java/com/nesrux/ecommerce/criteria/BasicoCriteriaApiTest.java
@@ -1,7 +1,6 @@
 package com.nesrux.ecommerce.criteria;
 
 import com.nesrux.ecommerce.model.Pedido.Pedido;
-import com.nesrux.ecommerce.model.cliente.Cliente;
 import org.junit.Assert;
 import org.junit.Test;
 import util.EntityManagerTest;
@@ -10,21 +9,22 @@ import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.math.BigDecimal;
 import java.util.List;
 
 public class BasicoCriteriaApiTest extends EntityManagerTest {
     @Test
     public void selecionarAtributoParaRetorno() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Cliente> criteriaQuery = criteriaBuilder.createQuery(Cliente.class);
+        CriteriaQuery<BigDecimal> criteriaQuery = criteriaBuilder.createQuery(BigDecimal.class);
         Root<Pedido> root = criteriaQuery.from(Pedido.class);
 
-        criteriaQuery.select(root.get("cliente"))
+        criteriaQuery.select(root.get("total"))
                 .where(criteriaBuilder.equal(root.get("id"), 1));
 
-        TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
-        Cliente cliente = typedQuery.getSingleResult();
-        Assert.assertNotNull(cliente);
+        TypedQuery<BigDecimal> typedQuery = entityManager.createQuery(criteriaQuery);
+        BigDecimal valor = typedQuery.getSingleResult();
+        Assert.assertEquals(new BigDecimal("2398.00"), valor);
     }
 
 
