295f790363bc808a6019f117a1735431bb931bea
Nesrux <joaomarcosdevs@gmail.com>
2023-12-04 16:25:55 -0300

    Test: retornando propriedades de entidades com criteria api

--- src/test/java/com/nesrux/ecommerce/criteria/BasicoCriteriaApiTest.java
+++ src/test/java/com/nesrux/ecommerce/criteria/BasicoCriteriaApiTest.java
@@ -1,6 +1,7 @@
 package com.nesrux.ecommerce.criteria;
 
 import com.nesrux.ecommerce.model.Pedido.Pedido;
+import com.nesrux.ecommerce.model.cliente.Cliente;
 import org.junit.Assert;
 import org.junit.Test;
 import util.EntityManagerTest;
@@ -12,9 +13,23 @@ import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class BasicoCriteriaApiTest extends EntityManagerTest {
+    @Test
+    public void selecionarAtributoParaRetorno() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Cliente> criteriaQuery = criteriaBuilder.createQuery(Cliente.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root.get("cliente"))
+                .where(criteriaBuilder.equal(root.get("id"), 1));
+
+        TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
+        Cliente cliente = typedQuery.getSingleResult();
+        Assert.assertNotNull(cliente);
+    }
+
 
     @Test
-    public void buscarPorIdentificador(){
+    public void buscarPorIdentificador() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
         Root<Pedido> root = criteriaQuery.from(Pedido.class);
