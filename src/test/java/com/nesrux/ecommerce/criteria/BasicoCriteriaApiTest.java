c2647e77d4a59f0544f1887701fec4fb4e8e4e52
Nesrux <joaomarcosdevs@gmail.com>
2023-12-04 19:45:50 -0300

    Test: projetando resultados com tuplas

--- src/test/java/com/nesrux/ecommerce/criteria/BasicoCriteriaApiTest.java
+++ src/test/java/com/nesrux/ecommerce/criteria/BasicoCriteriaApiTest.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import util.EntityManagerTest;
 
+import javax.persistence.Tuple;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -15,18 +16,37 @@ import java.util.List;
 
 public class BasicoCriteriaApiTest extends EntityManagerTest {
     @Test
+    public void projetarTupla() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Tuple> query = criteriaBuilder
+                .createTupleQuery();
+
+        Root<Produto> root = query.from(Produto.class);
+
+        query.multiselect(root.get("id").alias("id"), root.get("nome").alias("nome"));
+
+        TypedQuery<Tuple> typedQuery = entityManager.createQuery(query);
+        List<Tuple> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(t -> System.out.println("ID : " + t.get("id") + " Nome: " + t.get("nome")));
+    }
+
+    @Test
     public void projetarResultado() {
         CriteriaQuery<Object[]> query = entityManager.getCriteriaBuilder()
                 .createQuery(Object[].class);
 
         Root<Produto> root = query.from(Produto.class);
+        //multi select serve para selecionar mais de uma propriedade,
+        //é tipo fazer um select p.nome, p.id from Pedido p
         query.multiselect(root.get("id"), root.get("nome"));
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(query);
         List<Object[]> lista = typedQuery.getResultList();
         Assert.assertFalse(lista.isEmpty());
 
-        lista.forEach(arr-> System.out.println(arr[0] + " -- " + arr[1]));
+        lista.forEach(arr -> System.out.println(arr[0] + " -- " + arr[1]));
     }
 
     @Test
